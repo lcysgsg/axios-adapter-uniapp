ac54f3fd6dde5e3cf4c6b1ae09f460cfb3ef84d2
lcysgsg <249313573@qq.com>
2020-10-16 17:07:17 +0800

    test

---
+++ __tests__/index.spec.js
@@ -0,0 +1,127 @@
+const axios = require('axios')
+const format = require('../helper/format')
+const isUploadFile = require('../helper/isUploadFile')
+
+function uniappAdapter_TEST(config = {}) {
+  return new Promise(function dispatchUniApp(resolve, reject) {
+    const uniConfig = format(config, resolve, reject)
+
+    resolve(uniConfig)
+  })
+}
+const instance = axios.create({
+  baseURL: 'URL.com',
+  adapter: uniappAdapter_TEST,
+})
+
+test('格式化 uniConfig', () => {
+  let config
+
+  config = {
+    method: 'GET',
+  }
+  instance(config).then((res) => {
+    expect(res.method).toBe('get')
+  })
+
+  config = {
+    method: 'POST',
+  }
+  instance(config).then((res) => {
+    expect(res.method).toBe('post')
+  })
+
+  config = {
+    url: '/user',
+    params: {
+      a: 1,
+      b: 2,
+    },
+  }
+  instance(config).then((res) => {
+    expect(res.method).toBe('get')
+    expect(res.url).toBe('URL.com/user?a=1&b=2')
+  })
+
+  config = {
+    method: 'post',
+    url: '/user',
+    params: {
+      a: 1,
+      b: 2,
+    },
+  }
+  instance(config).then((res) => {
+    expect(res.method).toBe('post')
+    expect(res.url).toBe('URL.com/user?a=1&b=2')
+  })
+
+  config = {
+    method: 'post',
+    url: '/user',
+    params: {
+      a: 1,
+      b: 2,
+    },
+    data: {
+      aa: 1,
+      bb: 2,
+    },
+    formData: {
+      aa: 1,
+      bb: 2,
+    },
+  }
+  instance(config).then((res) => {
+    expect(res.data).toEqual({
+      aa: 1,
+      bb: 2,
+    })
+    expect(res.url).toBe('URL.com/user?a=1&b=2')
+  })
+
+  config = {
+    method: 'post',
+    url: '/upload',
+    params: {
+      a: 1,
+      b: 2,
+    },
+    data: {
+      aa: 1,
+      bb: 2,
+    },
+    files: [{ name: 'file', uri: '/path' }],
+  }
+  instance(config).then((res) => {
+    expect(isUploadFile(res)).toEqual(true)
+    expect(res.formData).toEqual({
+      aa: 1,
+      bb: 2,
+    })
+    expect(res.url).toBe('URL.com/upload?a=1&b=2')
+  })
+
+  config = {
+    method: 'post',
+    url: '/upload',
+    params: {
+      a: 1,
+      b: 2,
+    },
+    data: {
+      aa: 1,
+      bb: 2,
+    },
+    filePath: '/path',
+    name: 'file',
+  }
+  instance(config).then((res) => {
+    expect(isUploadFile(res)).toEqual(true)
+    expect(res.formData).toEqual({
+      aa: 1,
+      bb: 2,
+    })
+    expect(res.url).toBe('URL.com/upload?a=1&b=2')
+  })
+})
