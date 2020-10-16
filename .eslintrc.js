38e857a9ad7c8a86dd22edbd74af36daf8ce6f1c
lcysgsg <249313573@qq.com>
2020-10-16 10:17:19 +0800

    add: eslint

---
+++ .eslintrc.js
@@ -0,0 +1,14 @@
+module.exports = {
+  root: true,
+  env: {
+    browser: true,
+    es2020: true,
+    node: true,
+  },
+  extends: ['eslint:recommended'],
+  parserOptions: {
+    ecmaVersion: 11,
+    sourceType: 'module',
+  },
+  rules: {},
+}
